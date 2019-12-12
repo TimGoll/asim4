5e3d78acd29313776826c85931fa6d26c0f35b13
Tim Goll <tim@timgoll.de>
2019-12-12 11:19:25 +0100

    assignment3 task1

--- assignment02/task01/run.m
+++ assignment02/task01/run.m
@@ -4,10 +4,7 @@ close ALL;
 warning ('off','all');
 
 % load parameter
-Parameter_PC2
-
-% get model
-ueq_init = 0;
+Parameter;
 
 % init task
 path_arr = strsplit(mfilename('fullpath'), {'/', '\'});
@@ -16,24 +13,13 @@ disp('running ' + task_name);
 
 % set extra variables
 task_params = struct;
-task_params.J = 0.125;
-task_params.T = 293;
-task_params.F = 0;
-task_params.noise = 0.1; %0.02 
-task_params.noise_f = 20;
-task_params.noise_start = 0.4;
-
-
+task_params.I = 0;
+task_params.D = 0;
+task_params.P = 13e7;
+task_params.type = 1;
 
 % run simulation
-simOut = sim('SMA_spring', 'SimulationMode', 'normal');
-u = simOut.get('joule');
-y = simOut.get('displacement');
-t = simOut.get('time');
-
-u_0 = u(40:end) - u(39);
-y_0 = y(40:end) - y(39);
-
-% 2. 3 poles, 0 zeroes reaches 95%
-% 3. J_amp = 0.1 --> 3 poles, 0 zeroes reaches 65.5%, 2 poles, 1 zero
-% reaches 91%
+simOut = sim('DE_mass', 'SimulationMode', 'normal');
+%u = simOut.get('joule');
+%y = simOut.get('displacement');
+%t = simOut.get('time');

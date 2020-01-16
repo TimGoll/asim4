ffe0bc75dbc975fdbca4e7ae7de49b360231185d
Tim Goll <tim@timgoll.de>
2020-01-16 11:18:55 +0100

    comparison pid/slide

--- assignment04/task01/run.m
+++ assignment04/task01/run.m
@@ -11,16 +11,38 @@ path_arr = strsplit(mfilename('fullpath'), {'/', '\'});
 task_name = string(path_arr(end-1));
 disp('running ' + task_name);
 
+path_arr(end) = [];
+path_arr(end) = [];
+path_arr = [path_arr 'plots'];
+plot_path = strjoin(path_arr, '/');
+
 % internal params
-%base_p = 13e7;
-%base_t = 3.166 - 3.1375;
+a_0 = 14.93;
+b_0 = -0.2891;
+tau_star = 0.02;
 
 % set extra variables
 task_params = struct;
-%task_params.I = 1.2 * base_p / base_t;
-%task_params.D = 0.075 * base_p * base_t;
-%task_params.P = 0.6 * base_p;
-%task_params.type = 1;
+task_params.I = a_0 / (tau_star * b_0);
+task_params.D = 0;
+task_params.P = 1 / (tau_star * b_0);
+
+task_params.sine_A = 0.0005;
+task_params.sine_f = 1;
+task_params.sine_bias = 0.103;
+
+task_params.J_max = 0.25;
+task_params.epsilon = 0.005;
 
 % run simulation
-simOut = sim('DE_mass', 'SimulationMode', 'normal');
+simOut = sim('SMA_spring_pid', 'SimulationMode', 'normal');
+time_pid = simOut.get('t');
+ref = simOut.get('ref');
+disp_pid = simOut.get('l');
+
+simOut = sim('SMA_spring_sliding', 'SimulationMode', 'normal');
+time_sliding = simOut.get('t');
+disp_slide = simOut.get('l');
+
+paw({time_pid, time_pid, time_sliding}, {ref, disp_pid, disp_slide}, {'reference', 'pid', 'sliding mode'}, 'time [s]', 'diplacement [m]', task_name, 'pid_slide', plot_path, true, true);
+
---
+++ assignment04/util/paw.m
@@ -0,0 +1,31 @@
+function [] = paw(data_x, data_y, data_title, label_x, label_y, task, title, folder, do_legend, do_write)
+    do_legend = do_legend || false;
+    do_write  = do_write || false;
+
+    % create figute
+    this_fig = figure('Name', title);
+    hold on;
+
+    data_length = size(data_y, 2); %x is always the same
+
+    for i=1:1:data_length %iterate over plot array
+        plot(data_x{i}, data_y{i}, 'DisplayName', data_title{i}, 'Linewidth', 2);
+    end
+
+    grid;
+    grid minor;
+
+    xlabel(label_x);
+    ylabel(label_y);
+
+    if (do_legend)
+        legend show;
+    end
+
+    set(gca, 'FontSize', 18);
+
+    if (do_write)
+        path = folder + "/" + task + "___" + strrep(title, ' ', '_') + ".png";
+        saveas(this_fig, path);
+    end
+end

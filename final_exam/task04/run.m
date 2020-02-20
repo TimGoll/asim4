4ef22de03bcdf2ad2b466eaf0a97b835edd302a5
Tim Goll <tim@timgoll.de>
2020-02-20 12:10:27 +0100

    PID tuning plots

--- final_exam/task04/run.m
+++ final_exam/task04/run.m
@@ -27,34 +27,25 @@ task_params.J2 = 0;
 task_params.u1 = 0;
 task_params.u2 = 1;
 
-task_params.noise_1 = 0;
-task_params.noise_1_f = 0;
-task_params.noise_1_start = 0;
+p_c = 0.6;
+T_c = 0.26;
 
-task_params.noise_2 = 1;
-task_params.noise_2_f = 1;
-task_params.noise_2_start = 0;
-task_params.noise_2_start_b = 8;
+task_params.P = 1 * p_c;
+task_params.I = 1.2 * p_c / T_c;
+task_params.D = 0.25 * p_c * T_c;
 
-delta = 0.707;
-                % noisy  - sine   - jumps
-k = 586.9;      % 586.9  - 675.49 -
-p1 = 11.89;     % 11.89  - 11.67  -
-p2 = 0.3653;    % 0.3653 - 0.4316 -
-
-task_params.P = p1^2 / (4 * k * delta^2);
-task_params.I = p2 * p1^2 / (4 * k * delta^2);
-task_params.D = 0;
+%task_params.P = 0.6 * p_c;
+%task_params.I = 1.2 * p_c / T_c;
+%task_params.D = 0.075 * p_c * T_c;
 
 % run simulation
-simOut = simulate('SMAWing_03_PID', do_rerun);
+simOut = simulate('SMAWing_04_PID', do_rerun);
 time = simOut.get('time');
 u1 = simOut.get('u1');
 u2 = simOut.get('u2');
 theta_1 = simOut.get('theta_1');
 theta_2 = simOut.get('theta_2');
-theta_ref = simOut.get('theta_ref'); 
+theta_ref = simOut.get('theta_ref');
 
-paw({time}, {theta_2}, {'theta_2'}, 'time [s]', 'theta', task_name, 'equilibrium_theta1_v2_PID_step', plot_path, true, true, {''}, 'southwest');
-paw({time}, {u2}, {'u_2'}, 'time [s]', 'u [W]', task_name, 'equilibrium_u2_v2_noise_PID_step', plot_path, true, true, {''}, 'southwest');
-paw({time}, {theta_2}, {'theta_2'}, 'time [s]', 'theta', task_name, 'equilibrium_theta1_v2_PID_step', plot_path, true, true, {''}, 'southwest');
+paw({time, time}, {theta_2, theta_ref}, {'theta_2', 'theta_{2,ref}'}, 'time [s]', 'theta', task_name, 'ziegler_nichols_theta2_controlled_with_d_tune2', plot_path, true, true, {'', ''}, 'southwest');
+paw({time}, {u2}, {'u_2'}, 'time [s]', 'u [W]', task_name, 'ziegler_nichols_u2_controlled_with_d_tune2', plot_path, true, true, {''}, 'southwest');
